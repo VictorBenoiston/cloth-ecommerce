86cc391692d9fd072b1ab54854050ffa853afa31
Victor Oliveira <63753669+VictorBenoiston@users.noreply.github.com>
2022-11-14 23:09:26 -0300

    This commit will update the redux to redux-thunk
    In this commit I updated the Redux, to Redux-Thunk. And now, I added a Spinner while the components load (categories only).

--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -1,16 +1,18 @@
-import { useContext, useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
 
 import ProductCard from '../../components/product-card/product-card.component.jsx';
-import { selectCategoriesMap } from '../../store/categories/category.selector.js';
+import Spinner from '../../components/spinner/spinner.component'
+
+import { selectCategoriesIsLoading, selectCategoriesMap } from '../../store/categories/category.selector.js';
 
 import './category.styles.scss';
 
 const Category = () => {
     const { category } = useParams();
     const categoriesMap = useSelector(selectCategoriesMap);
-    // const { categoriesMap } = useContext(CategoriesContext);
+    const isLoading = useSelector(selectCategoriesIsLoading);
     const [products, setProducts] = useState(categoriesMap[category]);
 
     useEffect(() => {
@@ -21,13 +23,13 @@ const Category = () => {
     return (
         <div className='category-container'>
             <h2 className='title'>{category.toUpperCase()}</h2>
-            <div className='category-container-cards'>
-                {products &&
-                    products.map((product) => <ProductCard key={product.id} product={product} />)
-                }
-            </div>
+            {isLoading ? (<Spinner />)
+                    : (<div className='category-container-cards'>
+                        {products &&
+                            products.map((product) => (<ProductCard key={product.id} product={product} />))
+                        }
+                    </div>)}
         </div>
-
     )
 
 }
--- src/store/categories/category.action.js
+++ src/store/categories/category.action.js
@@ -1,4 +1,31 @@
 import { CATEGORIES_ACTION_TYPE } from './category.types';
-import { createAction } from '../../utils/reducer/reducer.utils'
 
-export const setCategories = (categoriesArray) => createAction(CATEGORIES_ACTION_TYPE.SET_CATEGORIES, categoriesArray);
+import { createAction } from '../../utils/reducer/reducer.utils'
+import { getCategoriesAndDocuments } from '../../utils/firebase/firebase.utils'
+
+
+export const fetchCategoriesStart = () =>
+    createAction(CATEGORIES_ACTION_TYPE.FETCH_CATEGORIES_START);
+
+export const fetchCategoriesSuccess = (categoriesArray) =>
+    createAction(
+        CATEGORIES_ACTION_TYPE.FETCH_CATEGORIES_SUCCESS,
+        categoriesArray
+    );
+
+export const fetchCategoriesFailed = (error) =>
+    createAction(CATEGORIES_ACTION_TYPE.FETCH_CATEGORIES_FAILED, error);
+
+export const fetchCategortiesStartAsync = () => {
+    return async (dispatch) => {
+        dispatch(fetchCategoriesStart());
+        try {
+            // The fetching is now made here. In this case, we are accessing the 
+            // categories on the db.
+            const categoriesArray = await getCategoriesAndDocuments('categories');
+            dispatch(fetchCategoriesSuccess(categoriesArray));
+        } catch (error) {
+            dispatch(fetchCategoriesFailed(error));
+        }
+    };
+};
